89e820f39df24451a8a5a1ecd3f994997ccb07b3
Rex Kerr <ichoran@gmail.com>
2015-10-26 16:51:14 -0700

    Working on JSON output.  Will not compile--work in progress.

--- src/scala/src/main/scala/Json.scala
+++ src/scala/src/main/scala/Json.scala
@@ -2,6 +2,157 @@ package org.openworm.trackercommons.json
 
 import fastparse.all._
 
+class Indented(val text: String) {
+  private var myIndent: Int = 0
+  def indent = myIndent
+  def indent_=(i: Int) { myIndent = math.max(indent, 0) }
+  def textFn(f: String => String) = { val ans = new Indented(f(text)); ans.indent = i; ans }
+  def in(i: Int): this.type = { indent = indent + i; this }
+}
+object Indented {
+  def apply(text: String) = new Indented(text)
+}
+
+trait JsonOut {
+  def toJson: String
+  def toJsons: Vector[Indented] = Vector(Indented(toJson))
+}
+
+sealed trait JSON extends JsonOut {}
+
+case object NullJ extends JSON { def toJson = "null" }
+
+sealed trait BoolJ extends JSON { def value: Boolean }
+case object TrueJ extends BoolJ { 
+  def value = true
+  def toJson = "true" 
+}
+case object FalseJ extends BoolJ {
+  def value = false
+  def toJson = "false" 
+}
+
+case class StrJ(value: String) extends JSON {
+  def toJson: String = {
+    var n = 0
+    var i = 0
+    while (i < value.length) {
+      val c = value.charAt(i)
+      if (c >= 32 && c < 127)  { 
+        if (c != '"' && c != '\\') n += 1
+        else n += 2
+      }
+      else if (c == '\b' || c == '\f' || c == '\n' || c == '\r' || c == '\t') n += 2
+      else n += 6
+      i += 1
+    }
+    if (n == i) "\"" + value + "\""
+    else {
+      var ac = new Array[Char](n)
+      i = 0
+      n = 0
+      while (i < value.length) {
+        val c = value.charAt(i)
+        if (c >= 32 && c < 127) {
+          if (c != '"' && c != '\\') { ac(n) = '\\'; ac(n+1) = c; n += 2 }
+          else { ac(n) = c; n += 1 }
+        }
+        else {
+          ac(n) = '\\'
+          c match {
+            case '\b' => ac(n+1) = 'b'; n += 2
+            case '\f' => ac(n+1) = 'f'; n += 2
+            case '\n' => ac(n+1) = 'n'; n += 2
+            case '\r' => ac(n+1) = 'r'; n += 2
+            case '\t' => ac(n+1) = 't'; n += 2
+            case _ =>
+              ac(n+1) = 'u'
+              ac(n+2) = (c >> 12) match { case x if x < 10 => ('0' + x).toChar; case _ => ('A' + (x-10)).toChar }
+              ac(n+3) = ((c >> 8) & 0xF) match { case x if x < 10 => ('0' + x).toChar; case _ => ('A' + (x-10)).toChar }
+              ac(n+4) = ((c >> 4) & 0xF) match { case x if x < 10 => ('0' + x).toChar; case _ => ('A' + (x-10)).toChar }
+              ac(n+5) = (c & 0xF) match { case x if x < 10 => ('0' + x).toChar; case _ => ('A' + (x-10)).toChar }
+              n += 6
+          }
+        }
+        i += 1
+      }
+      new String(ac)
+    }
+  }
+}
+
+case class NumJ(value: Double) extends JSON {
+  def toJson = if (value.isNaN || value.isInfinite) "null" else "%.4f".format(value)
+}
+
+case class ANumJ(values: Array[Double]) extends JSON {
+  def toJson = values.map(x => if (value.isNaN || value.isInfinite) "null" else "%.4f".format(value)).mkString("[", ", ", "]")
+  def toJsons =
+    if (values.length < 20) Vector(Indented(toJson))
+    else {
+      val vb = Vector.newBuilder[Indented]
+      vb += Indented("[")
+      var i = 0
+      while (i < values.length) {
+        var k = i+20
+        var sb = new StringBuilder
+        while (i < k && i < values.length) {
+          val vi = values(i)
+          sb ++= (if (vi.isNaN || vi.isInfinite) "null" else "%.4f".format(vi))
+          if (i+1 < values.length) sb ++= ", "
+        }
+        if (k-i < 20) vb += Indented(sb.result()).in(2)
+      }
+      vb += Indented("]")
+      vb.result()
+    }
+}
+case class AANumJ(valuess: Array[Array[Double]]) extends JSON {
+  def toJson = valuess.map(x => ANumJ(x).toJson).mkString("[ ", ", ", " ]")
+  def toJsons = {
+    val vb = Vector.newBuilder[Indented]
+    vb += Indented("[")
+    var it = valuess.iterator
+    while (it.hasNext) {
+      val s = ANumJ(it.next).toJson
+      vb += Indented(if (it.hasNext) s + "," else s).in(2)
+    }
+    vb += Indented("]")
+    vb.result()
+  }
+}
+case class ArrJ(values: Array[JSON]) extends JSON {
+  def toJson = values.map(_.toJson).mkString("[ ", ", ", "]")
+  def toJsons = {
+    val vb = Vector.newBuilder[Indented]
+    vb += Indented("[")
+    val it = values.iterator
+    while (it.hasNext) {
+      val jt = it.next.toJsons.iterator
+      while (jt.hasNext) {
+        val x = jt.next.in(2)
+        if (!jt.hasNext && it.hasNext) vb += x.textFn(_ + ",") else vb += x
+      }
+    }
+    vb.result()
+  }
+}
+case class ObjJ(keyvals: Map[String, Vector[JSON]]) extends JSON {
+  def toJson = keyvals.toVector.sortBy(_._1).flatMap{ case (k,vs) => vs.map(v => "\"" + k + "\": " + v.toJson) }.mkString("{ ", ", ", " }")
+  def toJsons =
+    if (keyvals.forall{ case (_, vs) => vs.length == 1 && vs match { case NullJ | _: BoolJ | _: NumJ | _: StrJ => true; case _ => false } }) Vector(Indented(toJson))
+    else {
+      val vb = Vector.newBuilder[Indented]
+      vb += Indented("{")
+      keyvals.toVector.sortBy(_._1).foreach{ case (k,vs) => vs.map(v => ???) }
+      vb += Indented("}")
+      vb.result()
+    }
+}
+case class TextJ(unparsed: String) extends JSON {
+  def toJson = unparsed
+}
+
 object Parts {
   val White = P( CharsWhile(_.isWhitespace) )
   def W[A](p: P[A]): P[A] = White.? ~ p ~ White.?
@@ -26,7 +177,7 @@ object Text {
     "\""
   )
 
-  val Num = ("-".? ~! ("0" ~! Pass | Digits) ~! ("." ~ Digits).? ~ (CharIn("eE") ~ CharIn("+-").? ~ Digits).?)
+  val Num = ("-".? ~ ("0" ~! Pass | Digits) ~! ("." ~ Digits).? ~ (CharIn("eE") ~ CharIn("+-").? ~ Digits).?)
 
   val Arr = P( "[" ~ W(Val.rep(sep = W(","))) ~ "]" )
 
@@ -40,22 +191,40 @@ object Text {
 object Data {
   import Parts._
 
-  val Null = Text.Null.map(_ => null)
-  val Bool = P("true").map(_ => true) | P("false").map(_ => false)
+  val Null = Text.Null.map(_ => NullJ)
+  val Bool = P("true").map(_ => TrueJ) | P("false").map(_ => FalseJ)
 
   val Esc = "\\" ~ (
     "u" ~ (Hex ~ Hex ~ Hex ~ Hex).!.map(h4 => java.lang.Integer.parseInt(h4, 16).toChar.toString) |
     CharIn("bfnrt/\\\"").!.map{ case "b" => "\b"; case "f" => "\f"; case "n" => "\n"; case "r" => "\r"; case "t" => "\t"; case x => x }
   )
-  val Str = "\"" ~ (CharsWhile(c => c != '"' && c != '\\').! | Esc).rep.map(_.mkString) ~ "\""
+  val Str = "\"" ~ (CharsWhile(c => c != '"' && c != '\\').! | Esc).rep.map(xs => StrJ(xs.mkString)) ~ "\""
 
-  val Num = Text.Num.!.map(_.toFloat)
+  val Dbl = 
+    Text.Num.!.map(_.toDouble) |
+    IgnoreCase("\"nan\"").map(_ => Double.NaN) |  // Not recommended, but valid JSON
+    IgnoreCase("nan").!.map(_ => Double.NaN) |    // Not even valid JSON, don't do this
+    (IgnoreCase("\"inf") ~ IgnoreCase("inity").? ~ "\"").map(_ => Double.PositiveInfinity) |   // Not recommended, but valid JSON
+    (IgnoreCase("inf") ~ IgnoreCase("inity").?).map(_ => Double.PositiveInfinity) |            // Not even valid JSON, don't do this
+    (IgnoreCase("\"-inf") ~ IgnoreCase("inity").? ~ "\"").map(_ => Double.NegativeInfinity) |  // Not recommended, but valid JSON
+    (IgnoreCase("-inf") ~ IgnoreCase("inity").?).map(_ => Double.NegativeInfinity)             // Not even valid JSON, don't do this
 
-  val Arr = P("[" ~ All.rep(sep = ",").map(_.toArray) ~ "]")
+  val Num = Dbl.map(x => NumJ(x))
 
-  val KeyVal = P(W(Str ~ W(":") ~! Val))
-  val Obj = "{" ~ KeyVal.rep(sep = ",").map(_.toMap) ~ "}"
+  val ADbl = "[" ~ W(Dbl).rep(sep = "," ~! Pass).map(_.toArray) ~ "]"
 
-  val Val: P[Any] = P( Obj | Arr | Str | Num | Bool | Null )
+  val ANum = ADbl.map(x => ANumJ(x))
+
+  val AADbl = "[" ~ W(ADbl).rep(1, sep = "," ~! Pass).map(_.toArray) ~ "]"
+
+  val AANum = AADbl.map(x => AANumJ(x))
+
+  val Arr = P("[" ~ All.rep(sep = ",").map(x => ArrJ(x.toArray)) ~ "]")
+
+  val KeyVal = P(W(Str.map(_.value) ~ W(":") ~! Val))
+  val Obj = "{" ~ KeyVal.rep(sep = ",").map(x => ObjJ(x.groupBy(_._1).map{ case (k,xs) => k -> xs.map(_._2).toVector })) ~ "}"
+
+  val Val: P[JSON] = P( Obj | NoCut(ANum) | NoCut(AANum) | Arr | Str | Num | Bool | Null )
+
   val All = W(Val)
 }
--- src/scala/src/main/scala/Parser.scala
+++ src/scala/src/main/scala/Parser.scala
@@ -20,8 +20,6 @@ object Parser {
     (java.time.LocalDateTime.of(y.toInt, mo.toInt, d.toInt, h.toInt, mi.toInt, ssi, ssns), loc.getOrElse(""))
   }
 
-  val QDate = "\"" ~ Date ~ "\""
-
   val Age = P((D rep 1).! ~ ":" ~ (D ~ D).! ~ ":" ~ (D ~ D ~ ("." ~ D.rep(1)).?).!).map{ case (h,m,s) => 
     val ss = s.toDouble
     val ssi = math.floor(ss).toInt
@@ -29,78 +27,16 @@ object Parser {
     java.time.Duration.ZERO.withSeconds(h.toLong*3600L + m.toInt*60 + ssi).withNanos(ssns)
   }
 
-  val QAge = "\"" ~ Age "\""
-
-  val Real = (
-    Data.Num |
-    Text.Null.map(_ => Double.NaN) |
-    IgnoreCase("\"nan\"").map(_ => Double.NaN) |  // Not recommended, but valid JSON
-    IgnoreCase("nan").!.map(_ => Double.NaN) |    // Not even valid JSON, don't do this
-    (IgnoreCase("\"inf") ~ IgnoreCase("inity").? ~ "\"").map(_ => Double.PositiveInfinity) |   // Not recommended, but valid JSON
-    (IgnoreCase("inf") ~ IgnoreCase("inity").?).map(_ => Double.PositiveInfinity) |            // Not even valid JSON, don't do this
-    (IgnoreCase("\"-inf") ~ IgnoreCase("inity").? ~ "\"").map(_ => Double.NegativeInfinity) |  // Not recommended, but valid JSON
-    (IgnoreCase("-inf") ~ IgnoreCase("inity").?).map(_ => Double.NegativeInfinity)             // Not even valid JSON, don't do this
-  )
-
-  val Realz = "[" ~ W(Real).rep(sep = "," ~! Pass).map(_.toArray) ~ "]"
-
-  val Reals = Real.map(x => Array(x)) | Realz
-
-  val Realzz = "[" ~ W(Reals).rep(sep = "," ~! Pass).map(_.toArray) ~ "]"
-
-  val Realss = NoCut(Realz.map(x => Array(x))) | Realzz
-
   val UnitKV = Data.Str ~ W(":" ~! Pass) ~ Data.Str
 
   val Units = {
     val u = new collection.mutable.AnyRefMap[String, units.Units]
     P(
       "\"units\"" ~ W(":") ~ "{" ~ 
-      W(UnitKV.filter{ case (k,v) => units.Units(v) match { case None => false; case Some(vi) => u += ((k, vi)); true }}).rep(sep = W(",")) ~
+      W(UnitKV.filter{ case (k,v) => units.Units(v.value) match { case None => false; case Some(vi) => u += ((k.value, vi)); true }}).rep(sep = W(",")) ~
       "}" 
     ).
     map(_ => u: collection.Map[String, units.Units]).
     filter(u => Seq("t", "x", "y").forall(u contains _))
-  }
-
-  def V[X](p: P[X]): P[Vector[X]] = "[" + p.rep(sep = "," ~! Pass).toVector + "]"
-
-  def Kw(s: String) = P(s ~ ":" ~! Pass)
-
-  val Meta = {
-    var lab = Vector.empty[Laboratory]
-    var who = Vector.empty[String]
-    var timestamp: Option[(java.time.LocalDateTime, String)] = None
-    var temperature: Option[Temperature] = None
-    var humidity: Option[Double] = None
-    var arena: Option[Arena] = None
-    var food: Option[String] = None
-    var media: Option[String] = None
-    var sex: Option[String] = None
-    var stage: Option[String] = None
-    var age: Option[java.time.Duration] = None
-    var strain: Option[String] = None
-    var protocol = Vector.empty[String]
-    var software: Option[Software] = None
-    var settings: Option[Any] = None
-    var custom: Map[String, Any] = None
-    P(
-      "\"metadata\"" ~ W(":" ~! Pass) ~ "{" ~ W(
-        (Kw("lab") ~ (Lab.map{l => lab += l; () } | V(Lab).map{ls => lab ++= ls; () })) |
-        (Kw("who") ~ (Str.map{s => who += s; () } | V(Str).map{ss => who ++= ss; () })) |
-        (Kw("timestamp") ~ QDate.filter(_ => timestamp.isEmpty).map{ case (ldt, s) => timestamp = Some((ldt, s)); ()}) |
-        (Kw("temperature") ~ Temp.filter(_ => temperature.isEmpty).map{t => temperature = Some(t); ()}) |
-        (Kw("humidity") ~ Data.Num.filter(_ => humidity.isEmpty))
-      ).rep("," ~! Pass) ~
-      "}"
-    ).map(_ => Metadata(lab, who, timestamp, temperature, humidity, arena, food, media, sex, stage, age, strain, protocol, software, settings, custom))
-  }
-
-  def apply(s: String) = {
-    var commons = false
-    var u: Option[collection.Map[String, units.Units]] = None
-    var m: Option[Metadata] = None
-    var d: Option[Data] = None
-    val at = new collection.mutable.AnyRefMap[String, Any]
   }
 }
