87073e8629eeac8ffdc47da0cd90ee63a45b86b0
Rex Kerr <ichoran@gmail.com>
2015-10-30 09:22:39 -0700

    Parsing whole files works (at least occasionally).

--- src/scala/src/main/scala/Parser.scala
+++ src/scala/src/main/scala/Parser.scala
@@ -20,6 +20,8 @@ object Parser {
     (java.time.LocalDateTime.of(y.toInt, mo.toInt, d.toInt, h.toInt, mi.toInt, ssi, ssns), loc.getOrElse(""))
   }
 
+  def durationFormat(jtd: java.time.Duration) = "%d:%02d:%07.4f".format(jtd.getSeconds/3600, (jtd.getSeconds % 3600)/60, (jtd.getSeconds % 60) + 1e-9*jtd.getNano)
+
   val Age = P((D rep 1).! ~ ":" ~ (D ~ D).! ~ ":" ~ (D ~ D ~ ("." ~ D.rep(1)).?).!).map{ case (h,m,s) => 
     val ss = s.toDouble
     val ssi = math.floor(ss).toInt
@@ -42,6 +44,8 @@ object Parser {
 
   val Meta = P("\"metadata\"" ~ W(":") ~! Struct.Obj)
 
+  val Files = P("\"files\"" ~ W(":") ~! Struct.Obj)
+
   val Dat = P(
     "\"data\"" ~ W(":") ~! (
       Struct.Obj.map(x => Array(x)) |
@@ -49,24 +53,57 @@ object Parser {
     )
   )
 
-  def Single = {
+  val Single = {
+    // Kludgy var-based method to parse something at most once
     var u: Option[Either[String, collection.Map[String, units.Units]]] = None
     var m: Option[Either[String, ObjJ]] = None
     var d: Option[Either[String, Array[ObjJ]]] = None
+    var f: Option[Either[String, ObjJ]] = None
     var commons = false
     val custom = collection.mutable.AnyRefMap.empty[String, List[JSON]]
     (
-      W("{") ~
+      W("{") ~! Pass.map{_ => u = None; m = None; d = None; f = None; commons = false; custom.clear(); () } ~
       P(
-        Commons.map{_ => commons = true; println("c"); () } |
+        Commons.map{_ => commons = true; () } |
+        Files.map{ x => f = if (f.isEmpty) Some(Right(x)) else Some(Left("Mulitple file specification blocks not supported")); () } |
         Units.map{ x => u = if (u.isEmpty) Some(Right(x)) else Some(Left("Multiple units blocks not supported")); () } |
         Meta.map{ x => m = if (m.isEmpty) Some(Right(x)) else Some(Left("Multiple metadata blocks not supported")); () } |
         Dat.map{ x => d = if (d.isEmpty) Some(Right(x)) else Some(Left("Multiple data blocks not supported (all should be in one array!)")); () } |
         Struct.KeyVal.map{ case (k,v) => if (k startsWith "@") { custom += (k, v :: custom.get(k).getOrElse(Nil)) }; () }
       ).rep(sep = W("," ~! Pass)) ~
       W("}")
-    ).map(_ => (u, m, d, commons, custom))
+    ).map(_ => (u, m, d, f, commons, custom))
   }
 
-  def durationFormat(jtd: java.time.Duration) = "%d:%02d:%07.4f".format(jtd.getSeconds/3600, (jtd.getSeconds % 3600)/60, (jtd.getSeconds % 60) + 1e-9*jtd.getNano)
+  def apply(s: String): Either[String, DataSet] = Single.parse(s) match {
+    case Result.Success((ou, om, od, ofs, com, cust), _) => (ou, od) match {
+      case (None, _) => Left("Not valid WCON--no units.")
+      case (_, None) => Left("Not valid WCON--no data.")
+      case (Some(Right(u)), Some(Right(d))) =>
+        val meta = om match {
+          case None => Metadata.empty
+          case Some(Left(l)) => return Left(l)
+          case Some(Right(m)) => (Metadata from m) match {
+            case Left(l) => return Left(l)
+            case Right(r) => r
+          }
+        }
+        val datas = d.map{ di => (DatX from di) match {
+          case Left(l) => return Left(l)
+          case Right(d)=> d
+        }}
+        val fs = ofs match {
+          case None => FileSet.empty
+          case Some(Left(l)) => return Left(l)
+          case Some(Right(f)) => (FileSet from f) match {
+            case Left(l) => return Left(l)
+            case Right(r) => r
+          }
+        }
+        Right(DataSet(meta, u, datas, fs, json.ObjJ(cust.toMap)))
+      case (Some(Left(l)), _) => Left(l)
+      case (_, Some(Left(l))) => Left(l)
+    }
+    case _ => Left("Parsing failed--probably not a valid JSON object?")
+  }
 }
