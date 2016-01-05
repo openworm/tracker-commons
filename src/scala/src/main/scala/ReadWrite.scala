fa8fecd6acfbdd0cc33f2be6e08f1a88b40ade3e
Rex Kerr <ichoran@gmail.com>
2016-01-05 13:44:51 -0800

    Modified FileSet to work better for large-scale multi-file records.

--- src/scala/src/main/scala/ReadWrite.scala
+++ src/scala/src/main/scala/ReadWrite.scala
@@ -1,1 +1,14 @@
+package org.openworm.trackercommons
 
+object ReadWrite {
+  def readOne(s: String): Either[String, DataSet] = readOne(new java.io.File(s))
+
+  def readOne(f: java.io.File): Either[String, DataSet] = ???
+
+  def readAll(s: String): Either[String, Array[DataSet]] = readAll(new java.io.File(s))
+
+  def readAll(f: java.io.File): Either[String, Array[DataSet]] = readOne(f) match {
+    case Left(err) => Left(err)
+    case Right(dset) => ???
+  }
+}
